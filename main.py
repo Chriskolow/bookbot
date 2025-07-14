6129de7b5270089fb0eca88d075747e47546e64c
Chriskolow <christoph.sebela@chello.at>
2025-07-14 10:59:07 +0200

    Added new function counting characters in a string

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import text_to_wordcount
+from stats import text_to_charactercount
 
 def get_book_text(path):
     with open(path) as f:
@@ -7,5 +8,6 @@ def get_book_text(path):
 
 def main():
     print(f"{text_to_wordcount(get_book_text("books/frankenstein.txt"))} words found in the document")
+    print(text_to_charactercount(get_book_text("books/frankenstein.txt")))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def text_to_wordcount(text):
     words = text.split()
     return len(words)
+
+def text_to_charactercount(text):
+    letters = {}
+    for letter in text:
+        if letter.lower() in letters:
+            letters[letter.lower()] += 1
+        else:
+            letters[letter.lower()] = 1
+    return letters

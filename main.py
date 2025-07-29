7a39901e921e7cc6f809576cc46a176e69bcc966
Chriskolow <christoph.sebela@chello.at>
2025-07-29 17:23:06 +0200

    Added Argument that multiple Books can be used

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import text_to_wordcount
 from stats import text_to_charactercount
 from stats import sort_charactercount
@@ -8,15 +9,19 @@ def get_book_text(path):
     return text
 
 def main():
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {text_to_wordcount(get_book_text("books/frankenstein.txt"))} total words")
-    #print(text_to_charactercount(get_book_text("books/frankenstein.txt")))
-    sort_characters = sort_charactercount(text_to_charactercount(get_book_text("books/frankenstein.txt")))
-    print("--------- Character Count -------")
-    for character in sort_characters:
-        if character["char"].isalpha():
-            print(f"{character["char"]}: {character["num"]}")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {text_to_wordcount(get_book_text(sys.argv[1]))} total words")
+        #print(text_to_charactercount(get_book_text("books/frankenstein.txt")))
+        sort_characters = sort_charactercount(text_to_charactercount(get_book_text(sys.argv[1])))
+        print("--------- Character Count -------")
+        for character in sort_characters:
+            if character["char"].isalpha():
+                print(f"{character["char"]}: {character["num"]}")
 
 main()
